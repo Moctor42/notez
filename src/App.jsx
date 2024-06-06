95fff94702ea94056fb62e9323343746ce60ab85
Moctor42 <moctor42@Ubuntu.myguest.virtualbox.org>
2024-06-06 15:33:14 +0100

    initial setup and some tailwind spiking

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div className='min-h-svh flex bg-slate-400'>
+      <h1 className='p-4 m-2 basis-0 bg-slate-300 flex font-sans text-5xl '>
+        Notez
+      </h1>
+    </div>
+  )
+}
+
+export default App
